ff719c9fc2529a0c09d7cb9b6310ccebdc8bca2a
Matías Martínez <83073528+xNanoEngine@users.noreply.github.com>
2023-09-05 17:29:52 -0300

    template/.env/fixed

---
+++ Backend/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from 'dotenv';
+
+//init
+dotenv.config();
+const app = express();
+
+// Import routes
+import userRoutes from "./routes/users.routes.js";
+import paperRoutes from "./routes/papers.routes.js";
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Configura CORS
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/papers", paperRoutes);
+
+export default app;
---
+++ Backend/src/persintence/database/database.js
@@ -0,0 +1,25 @@
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+// export const sequelize = new Sequelize(
+//   "test",
+//   "root",
+//   "",
+
+//   {
+//     host: "localhost",
+//     dialect: "mariadb",
+//   }
+// );
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mariadb",
+  }
+);
