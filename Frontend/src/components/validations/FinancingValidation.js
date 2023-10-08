f27ff6fb0ed542a886ba332f4ed9f1043ae81c66
xnanoengine <xnanoengine@gmail.com>
2023-10-08 20:32:44 -0300

    negative number validations bug

--- Frontend/src/components/validations/FinancingValidation.js
+++ Frontend/src/components/validations/FinancingValidation.js
@@ -4,5 +4,6 @@ export const FinancingSchema = Yup.object().shape({
   financing: Yup.string().required("Este campo es obligatorio."),
   budget: Yup.number()
     .typeError("Tiene que ser un numero")
+    .min(0, "Tiene que ser un número positivo")
     .required("El presupuesto es obligatorio."),
 });
