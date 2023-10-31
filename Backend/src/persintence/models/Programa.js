3ab380ceb133e99345bedc005ebea92f69c304d2
Sebastian <sebastian.paredes01@alumnos.uach.cl>
2023-10-31 14:55:23 -0300

    Datos comuna y programa

--- Backend/src/persintence/models/Programa.js
+++ Backend/src/persintence/models/Programa.js
@@ -91,8 +91,86 @@ const ProgramasToAdd = [
                     },
     ];
 
-Programa.bulkCreate(ProgramasToAdd).then(() => {
-    console.log('Registros creados exitosamente');
-    }).catch((error) => {
-    console.error('Error al crear registros:', error);
-});    
+Programa.afterSync(async () => {
+        try {
+        const programaExistente = await Programa.findOne({
+            where: { nombre: "Acciona" },
+        });  
+        if (!programaExistente) {
+            Programa.create({
+                nombre: "Acciona",
+                    descripcion: "Acciona es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                    url: "http://google.com",  
+            });
+            Programa.create({
+                nombre: "CECREA",
+                        descripcion: "CECREA es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Comunidad Creativa Regional",
+                        descripcion: "Comunidad Creativa Regional es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Cultura, Memoria y DDHH",
+                        descripcion: "Cultura, Memoria y DDHH es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Estrategias Regionales",
+                        descripcion: "Estrategias Regionales es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Fondo del Arte y la Educación",
+                        descripcion:  "Fondo del Arte y la Educación es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Fondos concursables",
+                        descripcion: "Fondos concursables es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Fondos concursables Industrias Creativas",
+                        descripcion: "Fondos concursables Industrias Creativas es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com",  
+            });
+            Programa.create({
+                nombre: "Fortalecimiento de la Identidad Cultural",
+                        descripcion: "Fortalecimiento de la Identidad Cultural es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Migrantes",
+                        descripcion: "Migrantes es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Plan Nacional de Lectura",
+                        descripcion: "Plan Nacional de Lectura es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Plan Nacional de Desarrollo Artístico en la región",
+                        descripcion: "Plan Nacional de Desarrollo Artístico en la región es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            Programa.create({
+                nombre: "Red Cultural",
+                        descripcion: "Red Cultural es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
+                        url: "http://google.com", 
+            });
+            console.log("datos de programa creados automáticamente.");
+        } else {
+            console.log("datos de programa ya existen.");
+        }
+        } catch (error) {
+        console.error(
+            "Error al crear el datos de programa automáticamente:",
+            error
+        );
+        }
+    });
+
