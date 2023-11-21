edd96b10fb56697a3c27ab77524a75977e083090
Sabadaba <83074056+Sabadaba@users.noreply.github.com>
2023-11-21 17:00:43 -0300

    Update initiatives.routes.js

--- Backend/src/routes/initiatives.routes.js
+++ Backend/src/routes/initiatives.routes.js
@@ -3,7 +3,6 @@ import {
   createIniciativa,
   getIniciativa,
   updateIniciativa,
-  deleteIniciativa,
   getIniciativas,
 } from "../controllers/initiatives.controller.js";
 import checkAuth from "../middleware/checkAuth.js";
@@ -12,7 +11,6 @@ const router = Router();
 // Routes
 router.post("/", checkAuth, createIniciativa);
 router.put("/:id", checkAuth, updateIniciativa);
-router.patch("/:id", checkAuth, deleteIniciativa);
 router.get("/", getIniciativas);
 router.get("/:id", getIniciativa);
 
