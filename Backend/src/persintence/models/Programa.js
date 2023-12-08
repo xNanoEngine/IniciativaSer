85ce42f410849e22c90d882ed4a6db4917137559
TukiTukk <82196770+TomasManriquez@users.noreply.github.com>
2023-12-08 18:32:43 -0300

    ruta de imagenes para cada programa

--- Backend/src/persintence/models/Programa.js
+++ Backend/src/persintence/models/Programa.js
@@ -16,7 +16,11 @@ export const Programa = sequelize.define("programa",{
     },
     url: {
         type: DataTypes.STRING,
-    }
+    },
+    imagen: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
 }, {
     timestamps: false,
     freezeTableName: true
@@ -26,68 +30,81 @@ const ProgramasToAdd = [
             {
                     nombre: "Acciona",
                     descripcion: "Acciona es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
-                    url: "http://google.com",    
+                    url: "http://google.com",   
+                    imagen:"Images/ACCIONA.png", 
                 },
     
                         {
                         nombre: "CECREA",
                         descripcion: "CECREA es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Comunidad Creativa Regional",
                         descripcion: "Comunidad Creativa Regional es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/acciona_redim.png", 
                     },
                         {
                         nombre: "Cultura, Memoria y DDHH",
                         descripcion: "Cultura, Memoria y DDHH es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Estrategias Regionales",
                         descripcion: "Estrategias Regionales es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Fondo del Arte y la Educación",
                         descripcion:  "Fondo del Arte y la Educación es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Fondos concursables",
                         descripcion: "Fondos concursables es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Fondos concursables Industrias Creativas",
                         descripcion: "Fondos concursables Industrias Creativas es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Fortalecimiento de la Identidad Cultural",
                         descripcion: "Fortalecimiento de la Identidad Cultural es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Migrantes",
                         descripcion: "Migrantes es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Plan Nacional de Lectura",
                         descripcion: "Plan Nacional de Lectura es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Plan Nacional de Desarrollo Artístico en la región",
                         descripcion: "Plan Nacional de Desarrollo Artístico en la región es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
                         {
                         nombre: "Red Cultural",
                         descripcion: "Red Cultural es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
                     },
     ];
 
@@ -100,67 +117,80 @@ Programa.afterSync(async () => {
             Programa.create({
                 nombre: "Acciona",
                     descripcion: "Acciona es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
-                    url: "http://google.com",  
+                    url: "http://google.com", 
+                    imagen:"Images/ACCIONA.png",  
             });
             Programa.create({
                 nombre: "CECREA",
                         descripcion: "CECREA es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
-                        url: "http://google.com", 
+                        url: "http://google.com",
+                        imagen:"Images/CECREA.png",  
             });
             Programa.create({
                 nombre: "Comunidad Creativa Regional",
                         descripcion: "Comunidad Creativa Regional es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Cultura, Memoria y DDHH",
                         descripcion: "Cultura, Memoria y DDHH es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Estrategias Regionales",
                         descripcion: "Estrategias Regionales es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Fondo del Arte y la Educación",
                         descripcion:  "Fondo del Arte y la Educación es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Fondos concursables",
                         descripcion: "Fondos concursables es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Fondos concursables Industrias Creativas",
                         descripcion: "Fondos concursables Industrias Creativas es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com",  
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Fortalecimiento de la Identidad Cultural",
                         descripcion: "Fortalecimiento de la Identidad Cultural es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Migrantes",
                         descripcion: "Migrantes es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Plan Nacional de Lectura",
                         descripcion: "Plan Nacional de Lectura es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Plan Nacional de Desarrollo Artístico en la región",
                         descripcion: "Plan Nacional de Desarrollo Artístico en la región es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             Programa.create({
                 nombre: "Red Cultural",
                         descripcion: "Red Cultural es uno de los programas mas reconocidos a nivel pais, en el se pueden destacar diferentes proyectos como por ejemplo: Proyecto Seremi Cultura, Proyecto arboles UACH, entre otros.",
                         url: "http://google.com", 
+                        imagen:"Images/CECREA.png", 
             });
             console.log("datos de programa creados automáticamente.");
         } else {
@@ -173,4 +203,3 @@ Programa.afterSync(async () => {
         );
         }
     });
-
