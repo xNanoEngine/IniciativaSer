79218dda80ab68084c9b71e794da7ac1cea5e2e0
Nano <xnanoengine@gmail.com>
2024-03-21 20:58:30 -0300

    Despliege docker

--- Backend/src/persintence/database/database.js
+++ Backend/src/persintence/database/database.js
@@ -1,16 +1,6 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
-// export const sequelize = new Sequelize(
-//   "test",
-//   "root",
-//   "",
-
-//   {
-//     host: "localhost",
-//     dialect: "mariadb",
-//   }
-// );
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
