5f05585ff5e0cc93e5e4f6dab8c9adf9f26d737b
Sebastian <sebastian.paredes01@alumnos.uach.cl>
2024-04-04 22:43:58 -0300

    Test

--- Backend/tests/AddIniciative.js
+++ Backend/tests/AddIniciative.js
@@ -9,13 +9,27 @@ async function example() {
     // await driver.wait(until.titleIs("ejemplo - Google"), 1000);
     // let title = await driver.getTitle();
     // console.log("Título de la página:", title);
-    await driver.get("http://198.58.113.82:3000/login");
+    await driver.get("http://localhost:3000/login");
     await driver.findElement(By.id("user_account")).sendKeys("admin");
     await driver
       .findElement(By.id("user_password"))
       .sendKeys("admin1122", Key.RETURN);
     // await driver.findElement(By.)
-    //await driver.findElement(By.css("Registrar Informacion")).click();
+    await driver.wait(until.elementLocated(By.xpath("//a[contains(@href, '/initiative')]")), 10000).click();
+
+    // Persona Juridica
+    await driver.wait(until.elementLocated(By.xpath("//span[text()='Personalidad Jurídica']")), 10000).click();
+    await driver.wait(until.elementLocated(By.xpath("//span[text()='Tipo de Persona Jurídica']")), 10000).click();
+    await driver.wait(until.elementLocated(By.xpath("//div[text()='Junta de vecinos']")), 10000).click();
+    await driver.findElement(By.id("name")).sendKeys("Mismisimo");
+    await driver.findElement(By.id("rut")).sendKeys("12.123.123-4");
+    await driver.wait(until.elementLocated(By.xpath("//span[text()='Rol Persona Jurídica']")), 10000).click();
+    await driver.wait(until.elementLocated(By.xpath("//div[text()='Ejecutor']")), 10000).click();
+    await driver.wait(until.elementLocated(By.xpath("//button[text()='Guardar']")), 10000).click();
+
+    // Persona Natural
+    await driver.wait(until.elementLocated(By.xpath("//span[text()='Persona Natural']")), 10000).click();
+
   } finally {
   }
 }
