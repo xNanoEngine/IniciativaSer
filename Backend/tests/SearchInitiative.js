5b06e4f87a26b923e97fe04aa5329fa0be744e3f
Javier Eduardo Mansilla Oyarzún <javieresky@gmail.com>
2024-04-06 20:07:30 -0300

    Added catch error

--- Backend/tests/SearchInitiative.js
+++ Backend/tests/SearchInitiative.js
@@ -1,9 +1,10 @@
 import { Builder, By, Key, until } from "selenium-webdriver";
 import {leerArchivoJSON} from "./OpenJson.js";
 import assert from 'assert';
+import { error } from "console";
 
 async function busquedaTest(ip, name, expectedName) {
-  let driver = await new Builder().forBrowser("firefox").build();
+  let driver = await new Builder().forBrowser("chrome").build();
   try {
     await driver.get(ip);
     await driver.findElement(By.id("busqueda")).sendKeys(name, Key.RETURN); // crear iniciativa antes que se llame test
@@ -12,7 +13,11 @@ async function busquedaTest(ip, name, expectedName) {
       10000
     );
     await leerIniciativaSelect.click();
-  } finally {
+  }
+  catch (error) {
+    console.error('Error: La iniciativa no existe', error);
+}
+  finally {
     let nombreIniciativaEncontrado = await driver.findElement(By.xpath(`//h1[contains(text(), "${name}")]`)).getText();
     assert.strictEqual(nombreIniciativaEncontrado, expectedName, 'El nombre de la iniciativa no coincide con el valor esperado.');
     console.log('El nombre de la iniciativa coincide con el valor esperado:', expectedName);
