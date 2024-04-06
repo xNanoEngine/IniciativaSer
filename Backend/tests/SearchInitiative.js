9eddd3746f124c871417142a01447170ffe915fa
Javier Eduardo Mansilla Oyarzún <javieresky@gmail.com>
2024-04-06 20:04:54 -0300

    Added assert

--- Backend/tests/SearchInitiative.js
+++ Backend/tests/SearchInitiative.js
@@ -1,8 +1,9 @@
 import { Builder, By, Key, until } from "selenium-webdriver";
 import {leerArchivoJSON} from "./OpenJson.js";
+import assert from 'assert';
 
-async function busquedaTest(ip, name) {
-  let driver = await new Builder().forBrowser("chrome").build();
+async function busquedaTest(ip, name, expectedName) {
+  let driver = await new Builder().forBrowser("firefox").build();
   try {
     await driver.get(ip);
     await driver.findElement(By.id("busqueda")).sendKeys(name, Key.RETURN); // crear iniciativa antes que se llame test
@@ -12,10 +13,14 @@ async function busquedaTest(ip, name) {
     );
     await leerIniciativaSelect.click();
   } finally {
+    let nombreIniciativaEncontrado = await driver.findElement(By.xpath(`//h1[contains(text(), "${name}")]`)).getText();
+    assert.strictEqual(nombreIniciativaEncontrado, expectedName, 'El nombre de la iniciativa no coincide con el valor esperado.');
+    console.log('El nombre de la iniciativa coincide con el valor esperado:', expectedName);
+    await driver.quit();
   }
 }
 
 
 const dato = await leerArchivoJSON();
 
-busquedaTest(dato.ip, dato.name);
+busquedaTest(dato.ip, dato.name, "test");
