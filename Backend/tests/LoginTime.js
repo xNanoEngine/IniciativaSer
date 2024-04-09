0a463d7d2837c70dc7f24688b07f06150acac826
Sebastian <sebastian.paredes01@alumnos.uach.cl>
2024-04-08 22:04:04 -0400

    test no funcional listo

--- Backend/tests/LoginTime.js
+++ Backend/tests/LoginTime.js
@@ -1,9 +1,11 @@
 import { Builder, By, Key } from "selenium-webdriver";
 import {leerArchivoJSON} from "./OpenJson.js";
+import assert from 'assert';
 
 
 async function loginTime(username, password) {
   let driver = await new Builder().forBrowser("chrome").build();
+  let elapsedTime;
   try {
     const startTime = new Date().getTime();
 
@@ -11,24 +13,46 @@ async function loginTime(username, password) {
     await driver.findElement(By.id("user_account")).sendKeys(username);
     await driver.findElement(By.id("user_password")).sendKeys(password, Key.RETURN);
 
+    await waitForUrlChange(driver, "http://localhost:3000", 5000);
+
     const endTime = new Date().getTime();
 
-    const elapsedTime = endTime - startTime;
+    elapsedTime = endTime - startTime;
 
     console.log(`Tiempo de carga: ` + elapsedTime.toString());
   } catch (error) {
     console.error(error);
   } finally {
-    const currentUrl = await driver.getCurrentUrl();
-    assert.strictEqual(currentUrl, 'http://localhost:3000/', 'La pagina resultado no es la de inicio.');
-    console.log('El valor obtenido coincide con el valor esperado:');
+    // const currentUrl = await driver.getCurrentUrl();
+    // console.log(currentUrl);
+    // assert.strictEqual(currentUrl, 'http://localhost:3000', 'La pagina resultado no es la de inicio.');
+    // console.log('El valor obtenido coincide con el valor esperado:');
     await driver.quit();
     return elapsedTime;
   }
 }
 
+async function waitForUrlChange(driver, targetUrl, timeout) {
+    return new Promise((resolve, reject) => {
+      const interval = 100; // Intervalo de verificación de la URL en milisegundos
+      let elapsedTime = 0;
+
+      const intervalId = setInterval(async () => {
+        elapsedTime += interval;
+        const currentUrl = await driver.getCurrentUrl();
+        if (currentUrl.includes(targetUrl)) {
+          clearInterval(intervalId);
+          resolve();
+        } else if (elapsedTime >= timeout) {
+          clearInterval(intervalId);
+          reject(new Error(`La página no cargó la URL ${targetUrl} dentro de ${timeout} milisegundos.`));
+        }
+      }, interval);
+    });
+  }
+
 const dato = await leerArchivoJSON("TestUsers.json");
-const tiempo = 0;
-for (const key in dato) {
-    tiempo += loginTime(key.name, key.password)}
+let tiempo = 0;
+for (let { name, password } of dato) {
+    tiempo += loginTime(name, password)}
 console.log("Tiempo promedio de carga: " + (tiempo/dato.length));
